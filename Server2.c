1448598ba48ed6f4a609ef6a9159c7df1c6c258c
Hridyanshu <hridyanshusharma@gmail.com>
2020-12-02 20:58:39 +0530

    Added necessary variable declarations

--- Server2.c
+++ Server2.c
@@ -18,3 +18,15 @@ char response[] = "HTTP/1.1 200 OK\r\n"
 "h1 { font-size:4cm; text-align: center; color: black;"
 " text-shadow: 0 0 2mm red}</style></head>"
 "<body><h1>Hii, world!</h1></body></html>\r\n";
+
+
+int main()
+{
+	/*Declaring necessary variables
+	one -> to be used in the function setsockopt
+	client_fd -> used to store the result of the accept method
+	svr_addr -> used to store the server address
+	cli_addr -> used to store the client address*/
+	int one = 1, client_fd;
+	struct sockaddr_in svr_addr, cli_addr;
+	socklen_t sin_len = sizeof(cli_addr);
