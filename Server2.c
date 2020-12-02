3a9cc48ba5054c3de751e21cd208ea2f26d29316
Hridyanshu <hridyanshusharma@gmail.com>
2020-12-02 20:57:28 +0530

    Response webpage to be displayed when server is visited

---
+++ Server2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <err.h>
+
+
+/* Storing the response webpage in a character array.
+This webpage will be displayed when client visits the server.*/
+char response[] = "HTTP/1.1 200 OK\r\n"
+"Content-Type: text/html; charset=UTF-8\r\n\r\n"
+"<!DOCTYPE html><html><head><title>Server2</title>"
+"<style>body { background-color: #111 }"
+"h1 { font-size:4cm; text-align: center; color: black;"
+" text-shadow: 0 0 2mm red}</style></head>"
+"<body><h1>Hii, world!</h1></body></html>\r\n";
